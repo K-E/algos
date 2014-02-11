8ea1a9433858ef86a2885b50acd92d50e5b13491
K-E <kesivakumar@gmail.com>
2014-02-11 00:33:48 -0500

    leet code solutions

--- src/lc/singlenumber.java
+++ src/lc/singlenumber.java
@@ -7,7 +7,7 @@ public class singlenumber {
 
 	public static void main(String[] args) {
 		int[] A	= {2,2,3,3,4,5,5,6,6,11,11,7};
-		ArrayList<int> hm	= new ArrayList<int>(Arrays.asList(A));
+		ArrayList hm	= new ArrayList(Arrays.asList(A));
 		System.out.println(hm.toString());
 		for(int i=0;i<hm.size();i++)
 		{
